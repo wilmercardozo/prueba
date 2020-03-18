21ab001957e6aa4e00c1b33b29d22657480ff731
WilmerCardozo <wilmer.cardozop@gmail.com>
2020-03-18 15:06:34 -0500

    tipos de via

---
+++ tipo-via/src/main/java/com/example/tipovia/TipoViaApplication.java
@@ -0,0 +1,24 @@
+package com.example.tipovia;
+
+import com.example.tipovia.config.SwaggerConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableJpaRepositories("com.example.tipovia")
+@EnableJpaAuditing
+@Import(SwaggerConfiguration.class)
+@EnableDiscoveryClient
+public class TipoViaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipoViaApplication.class, args);
+	}
+
+}
---
+++ tipo-via/src/test/java/com/example/tipovia/TipoViaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tipovia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipoViaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
